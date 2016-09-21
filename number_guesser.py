d80060d2b0267243cc7229d2befb0b82667ee8a6
williamjohngardner <bill@williamjohngardner.com>
2016-09-21 16:10:59 -0400

    Normal mode as OOP Class complete

--- number_guesser.py
+++ number_guesser.py
@@ -10,7 +10,6 @@ class NumberGuesser:
     def __init__(self):
         self.number = random.randint(1, 100)
         self.counter = 0
-
         print("""    *****************************
             *****************************
             *****************************
@@ -18,8 +17,44 @@ class NumberGuesser:
             *****************************
             *****************************
             *****************************""")
+        print(self.number)
 
-    def playagain():
+    def guess(self):
+        self.guess = int(input("PICK A NUMBER BETWIXT 1 & 100: "))
+        self.initial_guess()
+
+    def initial_guess(self):
+        if self.number == self.guess:
+            self.counter += 1
+            print("You guessed correctly on the first try.  Good job, Cheater.")
+            print("It took you " + str(self.counter) + " guesses to guess the number.  Cheater.")
+            self.playagain()
+        else:
+            self.game_play()
+
+    def game_play(self):
+        while self.guess != self.number:
+            if self.guess > self.number:
+                print("Your guess was too high. Guess again.")
+                self.counter += 1
+                self.guess = int(input("PICK ANOTHER NUMBER BETWIXT 1 & 100: "))
+                continue
+
+            elif self.guess < self.number:
+                print("Your guess was too low. Guess again.")
+                self.counter += 1
+                self.guess = int(input("PICK ANOTHER NUMBER BETWIXT 1 & 100: "))
+                continue
+
+            else:
+                break
+
+        if self.guess == self.number:
+            print("Your guess was correct. You must have cheated. Cheater.")
+            print("It took you " + str(self.counter) + " guesses to guess the number.  Cheater.")
+            self.playagain()
+
+    def playagain(self):
         playagain = input("Would you like to play again?: ").lower()
 
         if playagain == "y":
@@ -28,38 +63,8 @@ class NumberGuesser:
             print("Fine!!! Cheater.")
             exit()
 
-    def game_play(self):
-        while self.guess != self.number:
-            if self.guess > self.number:
-                print("Your guess was too high. Guess again.")
-                self.counter += 1
-                guess = int(input("PICK ANOTHER NUMBER BETWIXT 1 & 100: "))
-
-            elif self.guess < self.number:
-                print("Your guess was too low. Guess again.")
-                self.counter += 1
-                guess = int(input("PICK ANOTHER NUMBER BETWIXT 1 & 100: "))
-
-            else:
-                break
-
-        if guess == number:
-            print("Your guess was correct. You must have cheated. Cheater.")
-            print("It took you " + str(counter) + " guesses to guess the number.  Cheater.")
-            playagain()
-
-    def initial_guess(self):
-        if self.number == self.guess:
-            self.counter += 1
-            print("You guessed correctly on the first try.  Good job, Cheater.")
-            print("It took you " + str(self.counter) + " guesses to guess the number.  Cheater.")
-            playagain()
-        else:
-            game_play()
-
-    def guess(self):
-        self.guess = int(input("PICK A NUMBER BETWIXT 1 & 100: "))
-        initial_guess()
-
 game = NumberGuesser()
 game.guess()
+game.initial_guess()
+game.game_play()
+game.playagain()
